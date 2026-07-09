d21300a7edca83af0f7161bc5c914d848b98a0b0
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-09 11:58:21 +0300

    Сделал вместо категории простоя планового или непланового свойство IsPlanned, которое будет верно для плановых простоев.

--- BinnoMetric/Models/DowntimeType.cs
+++ BinnoMetric/Models/DowntimeType.cs
@@ -3,6 +3,6 @@ public class DowntimeType
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Category { get; set; } // "Planned" или "Unplanned"
+    public bool IsPlanned { get; set; }
     public bool IsActive { get; set; } = true;
 }
