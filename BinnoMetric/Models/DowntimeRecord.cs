78cabaa1abaaea8722b6f6afe323cdbdb1603b7f
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-09 12:13:27 +0300

    Добавил модель данных простоя

---
+++ BinnoMetric/Models/DowntimeRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace BinnoMetric.Models;
+public class DowntimeRecord
+{
+    public int Id { get; set; }
+    public int ProductionRecordId { get; set; }
+    public int DowntimeTypeId { get; set; }
+    public int DurationMinutes { get; set; } // Длительность простоя в минутах
+
+    // Навигационные свойства
+    public ProductionRecord ProductionRecord { get; set; }
+    public DowntimeType DowntimeType { get; set; }
+}
