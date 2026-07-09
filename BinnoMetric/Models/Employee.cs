2da9dfc7bf041f5c3e493cf035df54b3e6af705d
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-09 11:52:11 +0300

    Создал модель данных сотрудника

---
+++ BinnoMetric/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace BinnoMetric.Models;
+public class Employee
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string ShortName { get; set; }
+    public bool IsActive { get; set; } = true;
+}
