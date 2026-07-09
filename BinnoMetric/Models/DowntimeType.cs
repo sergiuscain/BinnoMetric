e8dd836b4c81b29e8bb5248c9c764391582fb124
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-09 12:14:56 +0300

    Добавил комментарии

--- BinnoMetric/Models/DowntimeType.cs
+++ BinnoMetric/Models/DowntimeType.cs
@@ -2,7 +2,7 @@
 public class DowntimeType
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public bool IsPlanned { get; set; }
+    public string Name { get; set; } // "Обед", "МУ", "ГУ", "ТО", и т.д.
+    public bool IsPlanned { get; set; } // true - плановый, false - внеплановый
     public bool IsActive { get; set; } = true;
 }
