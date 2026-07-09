1bdfc2d5a5f5812149bd70c2fec55549d0369a1b
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-09 11:53:39 +0300

    добавил модель данных производственной линии

---
+++ BinnoMetric/Models/EquipmentLine.cs
@@ -0,0 +1,8 @@
+﻿namespace BinnoMetric.Models;
+public class EquipmentLine
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public bool IsActive { get; set; } = true;
+}
