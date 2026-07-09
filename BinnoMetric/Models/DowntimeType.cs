950eb60e6d64a7469882a2c2fbc61af0329023c9
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-09 11:56:21 +0300

    Создал модель данных для типа простоя (типы, например МО (межсерийная очистка), ЕО(ежедневная очистка) и прочее. И каждый тип имеет категорию Planned или Unplanned )

---
+++ BinnoMetric/Models/DowntimeType.cs
@@ -0,0 +1,8 @@
+﻿namespace BinnoMetric.Models;
+public class DowntimeType
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; } // "Planned" или "Unplanned"
+    public bool IsActive { get; set; } = true;
+}
