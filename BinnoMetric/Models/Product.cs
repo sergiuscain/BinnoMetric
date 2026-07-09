eb15cc84311ffaf67023c2ff1bb0549b6faae3ac
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-09 11:50:51 +0300

    Добавил модель данных продукта

---
+++ BinnoMetric/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BinnoMetric.Models;
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ShortName { get; set; }
+    public string Dosage { get; set; }
+    public string Form { get; set; }
+    public int? PackSize { get; set; }
+    public bool IsActive { get; set; } = true;
+}
