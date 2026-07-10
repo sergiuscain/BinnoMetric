e09f3e8afe02a0f727ff87fb04fffefbe0b236d6
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-10 18:21:56 +0300

    Создаю интерфейсы для CRUD операций

---
+++ BinnoMetric/Abstractions/ICanGet.cs
@@ -0,0 +1,6 @@
+﻿namespace BinnoMetric.Abstractions;
+public interface ICanGet<T>
+{
+    public Task<T> GetAsync(string id);
+    public Task<ICollection<T>> GetAllAsync();
+}
--- BinnoMetric/Controllers/EmployeesController.cs
+++ BinnoMetric/Controllers/EmployeesController.cs
@@ -14,7 +14,7 @@ namespace BinnoMetric.Controllers
         {
             _employeesService = employeesService;
         }
-        [HttpPost]
+        [HttpPost("Add")]
         public async Task<IActionResult> AddEmployee(Employee employee)
         {
             await _employeesService.AddEmployee(employee);
