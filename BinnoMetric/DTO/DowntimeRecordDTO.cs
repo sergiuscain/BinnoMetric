b1aa53edce2825549cbba4cc2add3077eb98c166
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-13 11:41:20 +0300

    Создал DTO для записи простоя

---
+++ BinnoMetric/DTO/DowntimeRecordDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace BinnoMetric.DTO;
+public class DowntimeRecordDTO
+{
+    public int ProductionRecordId { get; set; }
+    public int DowntimeTypeId { get; set; }
+    public int DurationMinutes { get; set; }
+}
