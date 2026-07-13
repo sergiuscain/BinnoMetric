5db633368d738eb3bfd41c8a9626c5d1ea0d211b
Sergey Korolev <sergiussmithe@gmail.com>
2026-07-13 11:39:48 +0300

    Создал DTO для типа простоя

---
+++ BinnoMetric/DTO/DowntimeTypeDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace BinnoMetric.DTO;
+public class DowntimeTypeDTO
+{
+    public string Name { get; set; } // "Обед", "МУ", "ГУ", "ТО", и т.д.
+    public bool IsPlanned { get; set; } // true - плановый, false - внеплановый
+}
