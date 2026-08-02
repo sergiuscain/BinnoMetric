ac3ec673f4103d1f0252e1df57b1d9025056e78e
Sergey Korolev <sergiussmithe@gmail.com>
2026-08-02 13:20:00 +0300

    Рефакторинг.

--- BinnoMetric/DTO/ProductDTO.cs
+++ BinnoMetric/DTO/ProductDTO.cs
@@ -3,7 +3,4 @@ public class ProductDTO
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Dosage { get; set; }
-    public string Form { get; set; }
-    public int? PackSize { get; set; }
 }
