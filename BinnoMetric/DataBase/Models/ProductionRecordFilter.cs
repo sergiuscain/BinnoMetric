6ddea5680a6d5ffb432196baf86eb82d77865da8
Sergey Korolev <sergiussmithe@gmail.com>
2026-08-22 13:18:38 +0300

    Добавил оставшиеся фильтры

--- BinnoMetric/DataBase/Models/ProductionRecordFilter.cs
+++ BinnoMetric/DataBase/Models/ProductionRecordFilter.cs
@@ -2,7 +2,14 @@
 
 public class ProductionRecordFilter
 {
-    public int Page { get; set; }
-    public int PageSize { get; set; }
-    
+    public int? Page { get; set; }
+    public int? PageSize { get; set; }
+    public int? EmployeeId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public int? ProductId { get; set; }
+    public int? EquipmentLineId { get; set; }
+    public string? SeriesNumber { get; set; }
+    public int? ActualQuantity { get; set; }
+    public string? Comments { get; set; }
 }
--- BinnoMetric/Service/ProductionRecordsService.cs
+++ BinnoMetric/Service/ProductionRecordsService.cs
@@ -63,14 +63,63 @@ public class ProductionRecordsService
     {
         try
         {
-            var productionRecords = await _dBContext.ProductionRecords.Skip(filter.Page * filter.PageSize).Take(filter.PageSize).ToListAsync();
-            return productionRecords.Select(x => x.ToDTO()).ToList();
+            var productionRecords = await _dBContext.ProductionRecords.ToListAsync();
+            var filtredProductionRecords = Filter(productionRecords, filter);
+            return filtredProductionRecords.Select(x => x.ToDTO()).ToList();
         }
         catch
         {
             return null;
         }
     }
+
+    private List<ProductionRecord> Filter(List<ProductionRecord> productionRecords, ProductionRecordFilter filter)
+    {
+        var filtredProductionRecords = productionRecords;
+        if (filter.ProductId != null)
+        {
+            filtredProductionRecords = filtredProductionRecords.Where(x => x.ProductId == filter.ProductId).ToList();
+        }
+        if (filter.ActualQuantity != null)
+        {
+            filtredProductionRecords = filtredProductionRecords.Where(x => x.ActualQuantity == filter.ActualQuantity).ToList();
+        }
+        if (filter.EquipmentLineId != null)
+        {
+            filtredProductionRecords = filtredProductionRecords.Where(x => x.EquipmentLineId == filter.EquipmentLineId).ToList();
+        }
+        if (filter.SeriesNumber != null)
+        {
+            filtredProductionRecords = filtredProductionRecords.Where(x => x.SeriesNumber == filter.SeriesNumber).ToList();
+        }
+        if (filter.EmployeeId != null)
+        {
+            filtredProductionRecords = filtredProductionRecords
+                .Where(x => x.OperatorDId == filter.EmployeeId 
+                    || x.OperatorNKLId == filter.EmployeeId 
+                    || x.SeniorOperatorId == filter.EmployeeId 
+                    || x.PackerId == filter.EmployeeId )
+                .ToList();
+        }
+        if (filter.StartTime != DateTime.MinValue)
+        {
+            filtredProductionRecords = filtredProductionRecords.Where(x => x.StartTime >= filter.StartTime).ToList();
+        }
+        if (filter.EndTime != DateTime.MinValue)
+        {
+            filtredProductionRecords = filtredProductionRecords.Where(x => x.EndTime <= filter.EndTime).ToList();
+        }
+        if (filter.Comments != null)
+        {
+            filtredProductionRecords = filtredProductionRecords.Where(x => x.Comments.Contains(filter.Comments)).ToList();
+        }
+        if (filter.Page  != null && filter.PageSize != null)
+        {
+            filtredProductionRecords = filtredProductionRecords.Skip(filter.Page.Value * filter.PageSize.Value).Take(filter.PageSize.Value).ToList();
+        }
+        return filtredProductionRecords;
+    }
+
     internal async Task<int> GetPageCount(int pageSize)
     {
         var totalCount = await _dBContext.ProductionRecords.CountAsync();
